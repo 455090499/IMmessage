4552b17aa6abb0f8205c72a3f4ee2bd0f244a996
hhd <455090499@qq.com>
2018-01-04 09:22:54 +0800

    latest2

--- app/src/main/java/cn/jit/immessage/Info1Activity.java
+++ app/src/main/java/cn/jit/immessage/Info1Activity.java
@@ -2,12 +2,17 @@ package cn.jit.immessage;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -18,12 +23,15 @@ import java.util.List;
 public class Info1Activity extends AppCompatActivity {
     private Button button2;
     private Button button3;
+    private Button button1;
     private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info1);
         button2=(Button)findViewById(R.id.info1_btn2);
+        button1=(Button)findViewById(R.id.info1_btn1);
+        button3=(Button)findViewById(R.id.info1_btn3);
         text=(TextView)findViewById(R.id.info1_tv1);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,12 +47,33 @@ public class Info1Activity extends AppCompatActivity {
                 dialog.show();
             }
         });
-        button2.setOnClickListener(new View.OnClickListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i,2);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(Info1Activity.this,LoginActivity.class);
                 startActivity(intent);
             }
         });
+    }
+    protected void onActivityResult(int requestCode,int resultCode,Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (data != null) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn,null,null,null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            ImageView imageView = (ImageView) findViewById(R.id.info1_im);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
     }
 }
