4e6ef9712d8b11d7370607b27ed49a78bae8b040
longkang <1104475626@qq.com>
2018-01-04 11:03:51 +0800

    nlk

--- app/src/main/java/cn/jit/immessage/Info2Activity.java
+++ app/src/main/java/cn/jit/immessage/Info2Activity.java
@@ -1,7 +1,10 @@
 package cn.jit.immessage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Info2Activity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class Info2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info2);
+        Button btn2=(Button)findViewById(R.id.info2_btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Info2Activity.this,Info3Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/cn/jit/immessage/Info3Activity.java
@@ -0,0 +1,73 @@
+package cn.jit.immessage;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class Info3Activity extends AppCompatActivity {
+private  TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info3);
+        Button btn1=(Button)findViewById(R.id.info3_btn1);
+        Button btn2=(Button)findViewById(R.id.info3_btn2);
+        Button btn3=(Button)findViewById(R.id.info3_btn3);
+        text=(TextView)findViewById(R.id.info3_tv1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i,2);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                DatePickerDialog dialog = new DatePickerDialog(Info3Activity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        c.set(year, monthOfYear, dayOfMonth);
+                        text.setText(DateFormat.format("yyy-MM-dd", c));
+                    }
+                }, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
+                dialog.show();
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Info3Activity.this,Info2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode,int resultCode,Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (data != null) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn,null,null,null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            ImageView imageView = (ImageView) findViewById(R.id.info3_im);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
+    }
+}
