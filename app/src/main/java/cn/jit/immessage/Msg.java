9c8bf613b7df9eb3070acfdc82439ddd5e2c1e45
liweiwei <1651399352@qq.com>
2018-01-06 09:13:22 +0800

    add ChatActivity.java

---
+++ app/src/main/java/cn/jit/immessage/Msg.java
@@ -0,0 +1,22 @@
+package cn.jit.immessage;
+
+/**
+ * Created by liweiwei on 2018/1/5.
+ */
+public class Msg {
+    public static final int TYPE_RECEIVCED = 0;
+    public static final int TYPE_SENT = 1;
+    private String content;
+    private int type;
+
+    public Msg(String content, int type) {
+        this.content = content;
+        this.type = type;
+    }
+    public String getContent() {
+        return content;
+    }
+    public int getType() {
+        return  type;
+    }
+}
