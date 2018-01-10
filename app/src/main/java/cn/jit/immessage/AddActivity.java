183519936956a86e8905316412ee4823aa1c1bfa
fyf <1136714194@qq.com>
2018-01-10 14:20:55 +0800

    989548665

--- app/src/main/java/cn/jit/immessage/AddActivity.java
+++ app/src/main/java/cn/jit/immessage/AddActivity.java
@@ -2,7 +2,9 @@ package cn.jit.immessage;
 
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,10 +12,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -31,6 +37,7 @@ public class AddActivity extends AppCompatActivity {
     private ImageButton btn2;
     private Button btn3;
     private EditText et1;
+    private ImageView im;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +48,11 @@ public class AddActivity extends AppCompatActivity {
         btn3=(Button)findViewById(R.id.add_btn2);
         et1=(EditText)findViewById(R.id.add_et1);
         lv1 = (ListView) findViewById(R.id.add_lv1);
+        im=(ImageView)findViewById(R.id.head);
+
+        //图片设置
+        StrictMode.setThreadPolicy(new
+                StrictMode.ThreadPolicy.Builder().detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build());
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,27 +64,29 @@ public class AddActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                BmobQuery<uinfo> bmobQuery = new BmobQuery<>();
-                bmobQuery.addWhereEqualTo("phone", et1.getText().toString());
-                bmobQuery.findObjects(new FindListener<uinfo>() {
+                BmobQuery<uinfo> bmobQuery5 = new BmobQuery<>();
+                bmobQuery5.addWhereEqualTo("phone", et1.getText().toString());
+                bmobQuery5.findObjects(new FindListener<uinfo>() {
                     @Override
                     public void done(List<uinfo> list, BmobException e) {
                         for (uinfo u1 : list) {
-                           /* BmobFile bmobfile = u1.getPhoto();
 
-                            if(bmobfile!=null)
-                                try {
+//                            BmobFile bmobfile = u1.getPhoto();
+//                            try {
+//                                String url = bmobfile.getFileUrl();
+//                                Log.e("1", url);
+//                                Bitmap bitmap = getBitmap(url);
+//                                im.setImageBitmap(bitmap);
+//                                } catch (IOException e2) {
+//                                    // TODO Auto-generated catch block
+//                                    e2.printStackTrace();
+//                                }
 
-                                    String url = bmobfile.getFileUrl();
-                                    Log.e("1", url);
-                                    Bitmap bitmap = getBitmap(url);
-                                    im.setImageBitmap(bitmap);
-                                } catch (IOException e2) {
-                                    // TODO Auto-generated catch block
-                                    e2.printStackTrace();
-                                }*/
+                            String s1="(";
+                            String s2=")";
                             String[] name = {u1.getNiconame()};
-                            String[] desc = {u1.getPhone()};
+                            String[] desc = {(s1.concat(u1.getPhone())).concat(s2)};
+
                             List<Map<String, Object>> listems = new ArrayList<Map<String, Object>>();
                             for (int i = 0; i < name.length; i++) {
                                 Map<String, Object> listem = new HashMap<String, Object>();
@@ -94,5 +108,22 @@ public class AddActivity extends AppCompatActivity {
 
 
 
+    }
+    public Bitmap getBitmap(String path) throws IOException {
+        try {
+            URL url = new URL(path);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(5000);
+            conn.setRequestMethod("GET");
+            if (conn.getResponseCode() == 200) {
+                InputStream inputStream = conn.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
     }
 }
