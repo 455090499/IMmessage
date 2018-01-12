79e47e173d10fd6c6a32d3d9e9bdd0ddf094c5bb
hhd <455090499@qq.com>
2018-01-12 13:32:20 +0800

    lbwnb

--- app/src/main/java/cn/jit/immessage/DemoFragment.java
+++ app/src/main/java/cn/jit/immessage/DemoFragment.java
@@ -25,8 +25,10 @@ import java.util.Map;
 
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.datatype.BmobFile;
+import cn.bmob.v3.datatype.BmobQueryResult;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.FindListener;
+import cn.bmob.v3.listener.SQLQueryListener;
 
 import static android.content.ContentValues.TAG;
 
@@ -67,61 +69,116 @@ public class DemoFragment extends Fragment {
         }
         if (type == 1)
         {
+            String bql ="select * from uinfo where phone in (select fphone from pfriend where phone = '"+Body1Activity.p1.getPhone()+"')";
+            BmobQuery<uinfo> query=new BmobQuery<uinfo>();
+//设置查询的SQL语句
+            query.setSQL(bql);
+            query.doSQLQuery(new SQLQueryListener<uinfo>(){
 
-
-            BmobQuery<pfriend> bmobQuery = new BmobQuery<>();
-            bmobQuery.addWhereEqualTo("phone",Body1Activity.p1.getPhone());
-            bmobQuery.findObjects(new FindListener<pfriend>() {
                 @Override
-                public void done(List<pfriend> list, BmobException e) {
-                    int j = 0;
-                    for (pfriend pf1 : list) {
+                public void done(BmobQueryResult<uinfo> result, BmobException e) {
+                    if(e ==null){
+                        List<Map<String, Object>> listems = new ArrayList<Map<String, Object>>();
 
-                        Body1Activity.ffd[j++] = new String(pf1.getFphone());
-                    }
-                    final List<Map<String, Object>> listems = new ArrayList<Map<String, Object>>();
+                        List<uinfo> list = (List<uinfo>) result.getResults();
 
-                    for (int i = 0; i < j; i++) {
-                        BmobQuery<uinfo> bmobQuery = new BmobQuery<>();
-                        bmobQuery.addWhereEqualTo("phone", Body1Activity.ffd[i]);
-                        bmobQuery.findObjects(new FindListener<uinfo>() {
-                            @Override
-                            public void done(List<uinfo> list, BmobException e) {
-
-                                for (uinfo uf1 : list) {
-                                    String name =uf1.getNiconame();
-                                    String desc =uf1.getPhone();
-
-
-                                        Map<String, Object> listem = new HashMap<String, Object>();
-                                        listem.put("name", name);
-                                        listem.put("desc", desc);
-                                    listems.add(listem);
-                                }
-
-
-
+                            for (uinfo uf1 : list) {
+                                Map<String, Object> listem = new HashMap<String, Object>();
+                                String name =uf1.getNiconame();
+                                String desc =uf1.getPhone();
+                                Log.e(TAG, "done:"+name);
+                                listem.put("name", name);
+                                listem.put("desc", desc);
+                                listems.add(listem);
                             }
-                        });
+
+                            SimpleAdapter simplead = new SimpleAdapter(getActivity(), listems,
+                                    R.layout.haoyou, new String[] { "name", "head", "desc" },
+                                    new int[] {R.id.name,R.id.head,R.id.desc});
+
+
+                            listView.setAdapter(simplead);
+                            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                                    Intent intent;
+                                    intent = new Intent(getActivity(),ChatActivity.class);
+                                    startActivity(intent);
+                                }
+                            });
+
+                    }else{
+                        Log.i("smile", "错误码："+e.getErrorCode()+"，错误描述："+e.getMessage());
                     }
-                    SimpleAdapter simplead = new SimpleAdapter(getActivity(), listems,
-                            R.layout.haoyou, new String[] { "name", "head", "desc" },
-                            new int[] {R.id.name,R.id.head,R.id.desc});
-                    listView.setAdapter(simplead);
-
-                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                        @Override
-                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-
-                            Intent intent;
-                            intent = new Intent(getActivity(),ChatActivity.class);
-                            startActivity(intent);
-                        }
-                    });
-
                 }
             });
+
+
+
+
+
+
+
+
+
+//
+//            BmobQuery<pfriend> bmobQuery = new BmobQuery<>();
+//            bmobQuery.addWhereEqualTo("phone",Body1Activity.p1.getPhone());
+//            bmobQuery.findObjects(new FindListener<pfriend>() {
+//                @Override
+//                public void done(List<pfriend> list, BmobException e) {
+//                    int j = 0;
+//                    for (pfriend pf1 : list) {
+//
+//                        Body1Activity.ffd[j++] = pf1.getFphone();
+//                    }
+//                    final List<Map<String, Object>> listems = new ArrayList<Map<String, Object>>();
+//
+//                    for (int i = 0; i < j; i++) {
+//                        BmobQuery<uinfo> bmobQuery = new BmobQuery<>();
+//                        bmobQuery.addWhereEqualTo("phone", Body1Activity.ffd[i]);
+//                        bmobQuery.findObjects(new FindListener<uinfo>() {
+//                            @Override
+//                            public void done(List<uinfo> list, BmobException e) {
+//                                final Map<String, Object> listem = new HashMap<String, Object>();
+//
+//                                for (uinfo uf1 : list) {
+//                                    String name =uf1.getNiconame();
+//                                    String desc =uf1.getPhone();
+//
+//
+//                                        listem.put("name", name);
+//                                        listem.put("desc", desc);
+//                                    listems.add(listem);
+//                                }
+//
+//
+//
+//                            }
+//                        });
+//                    }
+//
+//                    SimpleAdapter simplead = new SimpleAdapter(getActivity(), listems,
+//                            R.layout.haoyou, new String[] { "name", "head", "desc" },
+//                            new int[] {R.id.name,R.id.head,R.id.desc});
+//                    simplead.notifyDataSetChanged();
+//
+//                    listView.setAdapter(simplead);
+//                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                        @Override
+//                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//
+//
+//                            Intent intent;
+//                            intent = new Intent(getActivity(),ChatActivity.class);
+//                            startActivity(intent);
+//                        }
+//                    });
+//
+//                }
+//            });
 
         }
         if (type == 2)
@@ -146,6 +203,5 @@ public class DemoFragment extends Fragment {
 
         return view;
     }
-
 
 }
